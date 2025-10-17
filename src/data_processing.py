4f4a9687600b510f0a423f4116019f6b84eb79c1
Hao <52360303+hc-solver@users.noreply.github.com>
2025-10-16 21:14:50 -0700

    Update data_processing.py

--- src/data_processing.py
+++ src/data_processing.py
@@ -1,1 +1,9 @@
+import pandas as pd
 
+def load_and_clean_data(path):
+    df = pd.read_csv(path)
+    df['Age'].fillna(df['Age'].median(), inplace=True)
+    df['Embarked'].fillna(df['Embarked'].mode()[0], inplace=True)
+    df['Sex'] = df['Sex'].map({'male': 0, 'female': 1})
+    df['Embarked'] = df['Embarked'].map({'S': 0, 'C': 1, 'Q': 2})
+    return df
